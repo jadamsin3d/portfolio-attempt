4367f6f1bd162c8e583d771c71f8beaaeea5ffdb
Atoms <jadamsin3d@gmail.com>
2019-02-23 21:53:13 -0800

    newchanges

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySideNav").style.width = "250px";
+    document.getElementById("maincontent").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySideNav").style.width = "0";
+    document.getElementById("maincontent").style.marginLeft = "0";
+}
