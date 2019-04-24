3de79d0ac928778d6dee1ac566d87e28588d97c7
Atoms <jadamsin3d@gmail.com>
2019-04-23 19:17:03 -0700

    changes so far

--- js/app.js
+++ js/app.js
@@ -7,3 +7,15 @@ function closeNav() {
     document.getElementById("mySideNav").style.width = "0px";
     document.getElementById("maincontent").style.marginLeft = "0px";
 }
+
+function closeAltNav() {
+    document.getElementById("myAltSideNav").style.width = "0px";
+    document.getElementById("myAltSideNav").style.height = "0px";
+    document.getElementById("maincontent").style.marginTop = "0px";
+}
+
+function openAltNav() {
+    document.getElementById("myAltSideNav").style.width = "100%";
+    document.getElementById("myAltSideNav").style.height = "100%";
+    document.getElementById("maincontent").style.marginTop = "100%";
+}
