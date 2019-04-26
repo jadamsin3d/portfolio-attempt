a17e9a19f371caf263d04bf52f524358be5edc1d
Atoms <jadamsin3d@gmail.com>
2019-04-26 00:27:23 -0700

    900 media is good

--- js/app.js
+++ js/app.js
@@ -9,13 +9,14 @@ function closeNav() {
 }
 
 function closeAltNav() {
-    document.getElementById("myAltSideNav").style.width = "0px";
+    document.getElementById("myAltSideNav").style.marginBottom = "0px";
     document.getElementById("myAltSideNav").style.height = "0px";
-    document.getElementById("maincontent").style.marginTop = "0px";
+    document.getElementById("myAltSideNav").style.zIndex = "1";
 }
 
 function openAltNav() {
-    document.getElementById("myAltSideNav").style.width = "100%";
+    document.getElementById("myAltSideNav").style.top = "0";
+    document.getElementById("myAltSideNav").style.marginBottom = "100%";
     document.getElementById("myAltSideNav").style.height = "100%";
-    document.getElementById("maincontent").style.marginTop = "100%";
+    document.getElementById("myAltSideNav").style.zIndex = "2";
 }
